0cefaca53bf75bea0ed5ea80b3149863f17aaf10
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-04 17:22:40 +0200

    fix

--- js-snack7/main.js
+++ js-snack7/main.js
@@ -1,5 +1,10 @@
-const Nomi= ["marco","antonio","andrea","giovanni"];
-const Cognomi=[" Grimaldi"," Di Fraia"," Giannetta"," Sepe"]
-for(let i=0 ;i <Nomi.length; i++){
-    console.log(Nomi[i] + Cognomi[i]);
+const Nomi = ['Andrea', 'Giovanni', 'Alessandro',`Antonio`, `Alex`];
+const Cognomi = ['Di Fraia', 'De Antonio', 'Del Monaco',`Galli`,`Giannetta`];
+
+let Lista = [];
+for (let i = 0; i < Nomi.length; i++) {
+    const ArNomi = Math.floor(Math.random() * Nomi.length);
+    const ArCognomi = Math.floor(Math.random() * Cognomi.length);
+    Lista.push(`${Nomi[ArNomi]} ${Cognomi[ArCognomi]}`);
 }
+console.log(Lista);
