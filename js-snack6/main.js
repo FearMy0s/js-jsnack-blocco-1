1c4be1a228ff9891cce6d8dae8cbd5df35a9524a
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-04 16:52:19 +0200

    es7 finito

--- js-snack6/main.js
+++ js-snack6/main.js
@@ -1,5 +1,5 @@
-    const number1 = prompt("inserisci numero");
-    const number2 = prompt("inserisci numero");
+const number1 = prompt("inserisci numero");
+const number2 = prompt("inserisci numero");
 if(number1%2==0){
     console.log(number1);
 }
---
+++ js-snack7/main.js
@@ -0,0 +1,5 @@
+const Nomi= ["marco","antonio","andrea","giovanni"];
+const Cognomi=[" Grimaldi"," Di Fraia"," Giannetta"," Sepe"]
+for(let i=0 ;i <Nomi.length; i++){
+    console.log(Nomi[i] + Cognomi[i]);
+}
