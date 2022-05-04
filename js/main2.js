7010582e557113590ec458f4c9f4936a88eebf4e
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-04 12:41:26 +0200

    snack 5 finito

---
+++ js/main2.js
@@ -0,0 +1,8 @@
+let container = [];
+for(let i = 0 ; i < 3 ; i++){
+    const number = Number = (prompt("inserisci numero"));
+if(number%2==1){
+    container.push(number)
+}
+   }
+   console.log (container)
