e0ebf5d8fe660a3c3fb7d66e3212bfcf6f0bddad
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-04 12:43:28 +0200

    push

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,5 @@
 let container = [];
-for(let i = 0 ; i < 3 ; i++){
+for(let i = 0 ; i < 10 ; i++){
     const number = Number = (prompt("inserisci numero"));
 if(number%2==1){
     container.push(number)
