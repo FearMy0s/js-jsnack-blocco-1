5dde2791837507c31209205a87b52f8e79bcf476
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-04 11:57:19 +0200

    push

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const name1 = prompt("inserisci primo nome");
 const name2 = prompt ("inserisci secondo nome");
 if(name1.length > name2.length){
-    console.log(name1);
+    console.log(name1,name2);
 }else if (name1.length === name2.length){
     console.log("i nomi sono uguali");
 }
 else{
-    console.log(name2);
+    console.log(name2,name1);
 }
