9b1465991d25c32655b57b224473954609fce575
FearMy0s <mirko.giannetta96@gmail.com>
2022-05-05 09:48:21 +0200

    fix

--- js-snack8/main.js
+++ js-snack8/main.js
@@ -1,8 +1,8 @@
 const Numeri = [1,2,3,4,5,6,7,8,9,10]
 let somma = 0;
 for(let i = 0; i<Numeri.length;i++){
-    if(Numeri[i]%2==1){
-        somma += Numeri[i];
+    if(i%2==1){
+        somma = somma + Numeri[i];
     }
 }
 console.log(somma)
